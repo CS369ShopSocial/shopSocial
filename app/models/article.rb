3c18b0960269e2d9d285328ca3c76f9f1e499997
Nicholas Hyatt <nhyatt@wisc.edu>
2014-05-02 19:58:10 -0500

    took out has_attached_file article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,2 @@
 class Article < ActiveRecord::Base
- has_attached_file :photo, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/ 
 end
