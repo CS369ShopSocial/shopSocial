9be834a33a8b122030e9bb6c462f0de32017b8a2
Nicholas Hyatt <nhyatt@wisc.edu>
2014-05-11 13:26:38 -0500

    fixed config for s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,9 +85,9 @@ ShopSocial::Application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['ShopSocial'],
-    :access_key_id => ENV['AKIAJIHPVJFG2X2U7I2A'],
-    :secret_access_key => ENV['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']
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
   
