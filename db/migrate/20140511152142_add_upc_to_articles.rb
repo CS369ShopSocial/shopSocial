ae2cc07a164952236e7e03763ac19aa7196dddc9
Nicholas Hyatt <nhyatt@wisc.edu>
2014-05-11 13:07:28 -0500

    fixed upc to small int

--- db/migrate/20140511152142_add_upc_to_articles.rb
+++ db/migrate/20140511152142_add_upc_to_articles.rb
@@ -1,7 +1,5 @@
 class AddUpcToArticles < ActiveRecord::Migration
   def change
-    add_column :articles, :upc => false do |t|
-      t.integer :upc, :limit => 8
-    end
+    add_column :articles, :upc, :integer
   end
 end
